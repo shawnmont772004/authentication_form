683534872adf84678243c8aa89c1eb63569dcd4a
shawnmont772004 <21b47.shawn@sjec.ac.in>
2023-11-15 15:26:41 +0530

    header component, pages and routes via Link from react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import  Home  from "./pages/Home.jsx"
+import Signin from "./pages/signin.jsx"
+import Signup from "./pages/Signup.jsx"
+
+import Header from "./components/Header.jsx"
 function App() {
   return (
-    <div className="text-blue-800">App</div>
-  )
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/signin" element={ <Signin />} />
+          <Route path="/signup" element={ <Signup />}/>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+function Header() {
+  return (
+    <>
+    <div className="p-8 flex  justify-between items-center bg-black shadow-md  text-white ">
+        <div>
+            <h1 className="font-semibold text-xl">Registration.website</h1>
+        </div>
+        <div>
+           <ul className="flex sm:gap-32 gap-16 items-center " >
+                <li >
+                    <Link to="/" className="hidden sm:inline hover:bg-white hover:text-black p-3  rounded-lg">home</Link>
+                </li>
+                <li >
+                    <Link to="/signin" className="hover:bg-white hover:text-black p-3   rounded-lg mr-8">sign in</Link>
+                </li>
+           </ul>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Header
